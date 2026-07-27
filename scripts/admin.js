775b7a48ca6ad98fed19aad690f3367c0bd19e23
Aiden Thorpe <surzies@gmail.com>
2026-07-27 23:00:20 +1000

    vNull.Null.Null

---
+++ scripts/admin.js
@@ -0,0 +1,17 @@
+function adminLogin() {
+  const adminID = document.getElementById("password").value;
+  const mainContent = document.getElementById("adminOnly");
+  const loginForm = document.getElementById("loginForm");
+  const incorrectID = document.getElementById("incorrectID");
+  let isOwner = true;
+
+  adminUsers = ["ShortBoiAidz", "paulblart", "1"];
+
+  if (adminUsers.includes(adminID) || isOwner) {
+      loginForm.style.display = "none";
+      mainContent.style.display = "flex";
+    }
+  else {
+      incorrectID.style.display = "flex";
+    }
+  }
--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,9 +1,10 @@
 function message() {
   let message = document.getElementById("message").value;
 
-  const webhook = "https://discord.com/api/webhooks/1531250659134013582/UwdAddAf2j4zDecpGjyCsmK3oJkit-WB4_W3qYUaq8IbM27Qoa6n_pLdYVaB1ZHnZ9hU";
+  const webhook = "https://discord.com/api/webhooks/1531283800066228315/PL7PfF3UgCCxD87ksn-E_lTH9Q8K4VkAHy-2FnFKWnbNH_iR3XLnBnQ1DCqfX0RnnR4Q";
   const content = `${message}`;
   const request = new XMLHttpRequest();
+
   request.open("POST", webhook)
   request.setRequestHeader('Content-type', 'application/json');
   const params = {
@@ -11,3 +12,7 @@ function message() {
   }
   request.send(JSON.stringify(params));
 }
+
+// Test webhook: https://discord.com/api/webhooks/1531283800066228315/PL7PfF3UgCCxD87ksn-E_lTH9Q8K4VkAHy-2FnFKWnbNH_iR3XLnBnQ1DCqfX0RnnR4Q
+
+// Noodlebox webhook: https://discord.com/api/webhooks/1531250659134013582/UwdAddAf2j4zDecpGjyCsmK3oJkit-WB4_W3qYUaq8IbM27Qoa6n_pLdYVaB1ZHnZ9hU
