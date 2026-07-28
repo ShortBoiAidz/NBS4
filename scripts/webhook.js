9d1cd896c52b21aca4c5eb2b9feebb066f3850cb
Aiden Thorpe <surzies@gmail.com>
2026-07-28 12:30:59 +1000

    v4.8.2

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,7 +1,7 @@
 function message() {
   let message = document.getElementById("message").value;
+  let webhook = document.getElementById("webhookLink").value;
 
-  const webhook = "https://discord.com/api/webhooks/1531283800066228315/PL7PfF3UgCCxD87ksn-E_lTH9Q8K4VkAHy-2FnFKWnbNH_iR3XLnBnQ1DCqfX0RnnR4Q";
   const content = `${message}`;
   const request = new XMLHttpRequest();
 
@@ -12,7 +12,3 @@ function message() {
   }
   request.send(JSON.stringify(params));
 }
-
-// Test webhook: https://discord.com/api/webhooks/1531283800066228315/PL7PfF3UgCCxD87ksn-E_lTH9Q8K4VkAHy-2FnFKWnbNH_iR3XLnBnQ1DCqfX0RnnR4Q
-
-// Noodlebox webhook: https://discord.com/api/webhooks/1531250659134013582/UwdAddAf2j4zDecpGjyCsmK3oJkit-WB4_W3qYUaq8IbM27Qoa6n_pLdYVaB1ZHnZ9hU
