5f5fd13f235ba303489a101a65642a1acc2dce63
Aiden Thorpe <surzies@gmail.com>
2026-08-12 06:41:59 +1000

    fixed day counter issue

--- scripts/trackDay.js
+++ scripts/trackDay.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const startDate = new Date('2026-08-05');
+  const startDate = new Date('2026-08-05T00:00:00');
   const currentDate = new Date();
   const differenceInMs = currentDate - startDate;
   const msPerDay = 1000 * 60 * 60 * 24;
   const daysBetween = Math.floor(differenceInMs / msPerDay);
   const dayCount = document.getElementById("server-day-count");
-  
+
   if (dayCount) {
     dayCount.textContent = `${daysBetween}`;
   } else {
